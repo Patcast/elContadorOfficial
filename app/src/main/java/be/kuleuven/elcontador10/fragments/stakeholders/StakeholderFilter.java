2bb4d1fc470ac9b69c44ad9b7b6b6af81d341925
Patricio Cstilo <patricio.castillo2009@gmail.com>
2021-05-01 17:09:41 +0200

    All frames for our simplified app are ready, next I will start adjusting the navigation

--- app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderFilter.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderFilter.java
@@ -1,4 +1,4 @@
-package be.kuleuven.elcontador10;
+package be.kuleuven.elcontador10.fragments.stakeholders;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import be.kuleuven.elcontador10.R;
 
 /**
  * A simple {@link Fragment} subclass.
