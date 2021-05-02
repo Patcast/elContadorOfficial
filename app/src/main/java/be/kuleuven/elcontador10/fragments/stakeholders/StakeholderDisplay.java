94445746243c8ac22bdcc39cdc69fb4353545fbe
elYonno <78961541+elYonno@users.noreply.github.com>
2021-05-02 20:58:42 +0200

    Allows user to call stakeholders

--- app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderDisplay.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderDisplay.java
@@ -1,10 +1,17 @@
 package be.kuleuven.elcontador10.fragments.stakeholders;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -21,6 +28,8 @@ import be.kuleuven.elcontador10.background.interfaces.stakeholders.StakeholdersD
 public class StakeholderDisplay extends Fragment implements StakeholdersDisplayInterface {
     private MainActivity mainActivity;
     private String id;
+    private String phoneNo;
+    private int CALL_PERMISSION = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,17 +62,62 @@ public class StakeholderDisplay extends Fragment implements StakeholdersDisplayI
     public void display(Bundle bundle) {
         TextView name, role, phone, email, balance;
 
+        // initialise views variables
         name = requireView().findViewById(R.id.txtStakeholderDisplayName);
         role = requireView().findViewById(R.id.txtStakeholderDisplayRole);
         phone = requireView().findViewById(R.id.txtStakeholderDisplayPhoneNo);
         email = requireView().findViewById(R.id.txtStakeholderDisplayEmail);
         balance = requireView().findViewById(R.id.txtStakeholderDisplayBalance);
+        phoneNo = bundle.getString("phone");
 
+        // set views text
         name.setText(bundle.getString("name"));
         role.setText(bundle.getString("role"));
-        phone.setText(bundle.getString("phone"));
+        phone.setText(phoneNo);
         email.setText(bundle.getString("email"));
-        balance.setText("$"+bundle.getDouble("balance"));
+        balance.setText(String.format("$%s", bundle.getDouble("balance")));
+
+        // set onClickListeners
+        phone.setOnClickListener(this::onPhoneNumber_Clicked);
+    }
+
+    public void onPhoneNumber_Clicked(View view) {
+        if (ActivityCompat.checkSelfPermission(mainActivity,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            //call permission not granted yet
+            requestCallPermission();
+        }
+        else {
+            Intent call = new Intent(Intent.ACTION_CALL);
+            call.setData(Uri.parse("tel:" + phoneNo));
+            startActivity(call);
+        }
+    }
+
+    private void requestCallPermission() {
+        // request permission politely
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CALL_PHONE)) {
+            new AlertDialog.Builder(mainActivity)
+                    .setTitle("Call permission needed")
+                    .setMessage("Accept to allow phone calls from the app")
+                    .setPositiveButton("Ok", (dialog, which) -> requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, CALL_PERMISSION))
+                    .setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss())
+                    .create()
+                    .show();
+        }
+        else {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, CALL_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CALL_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                error("Permission granted.");
+                requestCallPermission();
+            } else error("Permission not granted.");
+        }
     }
 
     @Override
