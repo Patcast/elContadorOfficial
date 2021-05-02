c7dd1c26f1c026174ba57f51019719bdfda3a560
elYonno <78961541+elYonno@users.noreply.github.com>
2021-05-02 17:19:04 +0200

    CheckBoxes respond to All

--- app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderFilter.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderFilter.java
@@ -1,10 +1,14 @@
 package be.kuleuven.elcontador10.fragments.stakeholders;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -19,6 +23,8 @@ import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import be.kuleuven.elcontador10.R;
 import be.kuleuven.elcontador10.activities.MainActivity;
@@ -28,12 +34,18 @@ public class StakeholderFilter extends Fragment {
     private MainActivity mainActivity;
 
     private TextView name;
+
     private CheckBox all_roles;
     private LinearLayout chipGroup;
     private ArrayList<CheckBox> roles;
     private CheckBox inDebt;
+    private ArrayList<String> roleNames;
+    private int noOfChecked;
+
     private Button cancel;
     private Button filter;
+
+    private NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,6 +53,8 @@ public class StakeholderFilter extends Fragment {
         mainActivity = (MainActivity) requireActivity();
         mainActivity.setTitle("Filter Stakeholder");
         roles = new ArrayList<>();
+        roleNames = new ArrayList<>();
+        noOfChecked = 0;
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stakeholder_filter, container, false);
     }
@@ -58,23 +72,71 @@ public class StakeholderFilter extends Fragment {
         filter = requireView().findViewById(R.id.btn_filter_FilterStakeholder);
 
         // initialise chip group
-        String[] roleNames = getResources().getStringArray(R.array.roles);
+        // TODO get roles from database instead of local
+        String[] temp_array = getResources().getStringArray(R.array.roles);
+        roleNames.addAll(Arrays.asList(temp_array));
         setRoles(roleNames);
+
+        // set navigation
+        navController = Navigation.findNavController(view);
+        cancel.setOnClickListener(this::onClick_Cancel);
+        filter.setOnClickListener(this::onClick_Filter);
+
+        // roles CheckBox animation
+        all_roles.setOnCheckedChangeListener(this::onClick_All);
     }
 
-    private void setRoles(String[] role_names) {
+    public void onClick_Cancel(View view) {
+        FilterStakeholdersParcel filter = new FilterStakeholdersParcel("*", roleNames, false);
+        StakeholderFilterDirections.ActionStakeholderFilterToStakeholderSummary action =
+                StakeholderFilterDirections.actionStakeholderFilterToStakeholderSummary(filter);
+        navController.navigate(action);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void onClick_Filter(View view) {
+        StakeholderFilterDirections.ActionStakeholderFilterToStakeholderSummary action =
+                StakeholderFilterDirections.actionStakeholderFilterToStakeholderSummary(getFilter());
+        navController.navigate(action);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void onClick_All(View view, boolean checked) {
+        if (checked) roles.forEach(v -> v.setChecked(true));
+        else roles.forEach(v -> v.setChecked(false));
+    }
+
+    public void setRoles(ArrayList<String> role_names) {
         for (String tagName : role_names) {
             CheckBox chip = new CheckBox(getContext());
             chip.setText(tagName);
+            chip.setChecked(true);
+            chip.setOnCheckedChangeListener(this::onClick_Role);
 
+            noOfChecked++;
             roles.add(chip);
             chipGroup.addView(chip);
         }
     }
 
+    public void onClick_Role(View view, boolean checked) {
+        if (checked) noOfChecked++;
+        else noOfChecked--;
+
+        all_roles.setChecked(noOfChecked == roles.size()); // sets all roles: true if all checkboxes are checked
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public FilterStakeholdersParcel getFilter() {
         String name_text = name.getText().toString();
-        ArrayList<String> categories = new ArrayList<>(5);
+
+        // get the names of checked CheckBox
+        ArrayList<String> categories = roles.stream()
+                .filter(CheckBox::isChecked)
+                .map(v -> v.getText().toString())
+                .collect(Collectors.toCollection(ArrayList::new));
+        ;
+
         boolean debt = inDebt.isChecked();
 
         return new FilterStakeholdersParcel(name_text, categories, debt);
