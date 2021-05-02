c22609736a8f6b4ee5214490125f17b669062bfb
elYonno <78961541+elYonno@users.noreply.github.com>
2021-05-02 14:43:49 +0200

    setting up StakeholderFilter

--- app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderFilter.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/stakeholders/StakeholderFilter.java
@@ -2,65 +2,51 @@ package be.kuleuven.elcontador10.fragments.stakeholders;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 import be.kuleuven.elcontador10.R;
+import be.kuleuven.elcontador10.activities.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StakeholderFilter#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StakeholderFilter extends Fragment {
+    private MainActivity mainActivity;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public StakeholderFilter() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StakeholderFilter.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StakeholderFilter newInstance(String param1, String param2) {
-        StakeholderFilter fragment = new StakeholderFilter();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TextView name;
+    private Chip all_roles;
+    private ChipGroup roles;
+    private Chip inDebt;
+    private Button cancel;
+    private Button filter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mainActivity = (MainActivity) requireActivity();
+        mainActivity.setTitle("Filter Stakeholder");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stakeholder_filter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // set view variables
+        name = requireView().findViewById(R.id.stakeholder_filter_name);
+        all_roles = requireView().findViewById(R.id.stakeholder_filter_all);
+        roles = requireView().findViewById(R.id.stakeholder_filter_group);
+        inDebt = requireView().findViewById(R.id.stakeholder_filter_indebt);
+        cancel = requireView().findViewById(R.id.btn_cancel_FilterStakeholder);
+        filter = requireView().findViewById(R.id.btn_filter_FilterStakeholder);
     }
 }
