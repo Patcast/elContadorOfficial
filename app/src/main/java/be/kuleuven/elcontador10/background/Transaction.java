891ec380196fe6ed1d8c38ba55add781f3f14b39
Patricio Cstilo <patricio.castillo2009@gmail.com>
2021-05-06 12:56:39 +0200

    The stakeholder is correctly register in  newTransaction

--- app/src/main/java/be/kuleuven/elcontador10/background/Transaction.java
+++ app/src/main/java/be/kuleuven/elcontador10/background/Transaction.java
@@ -3,40 +3,49 @@ package be.kuleuven.elcontador10.background;
 public class Transaction {
     private boolean cashIn;
     private double amount;
-    private String person;
+    private int idStakeholder;
     private int idType;
     private String txtComments;
+
+
+    private final String idOfBudget = "8";
 
 
 
 
     ///Constructor use to make object to create submit URL
-    public Transaction(boolean cashIn, double amount, String person,int idTypeTrans, String txtComments) {
+    public Transaction(boolean cashIn, double amount, int idStake,int idTypeTrans, String txtComments) {
         this.cashIn = cashIn;
         this.amount = amount;
-        this.person = person;
+        this.idStakeholder = idStake;
         this.idType = idTypeTrans;
         this.txtComments = txtComments;
-    }
-
-    public boolean isCashIn() {
-        return cashIn;
     }
 
     public double getAmount() {
         return amount;
     }
 
-    public String getPerson() {
-        return person;
+    public int getIdStakeholder() {
+        return idStakeholder;
     }
 
-    public int getIdType() {
-        return idType;
+    public String getIdType() {
+        return String.valueOf(idType);
     }
 
     public String getTxtComments() {
         return txtComments;
+    }
+
+    public String getStakePays() {
+        if(cashIn){ return String.valueOf(idStakeholder); }
+        return idOfBudget;
+    }
+
+    public String getStakeReceives() {
+        if(cashIn){ return String.valueOf(idOfBudget); }
+        return String.valueOf(idStakeholder);
     }
 }
 
