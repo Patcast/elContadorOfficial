25d3385db3eae0c478f2dabecbfa00087296dd93
patocastillo <patricio.castillo2009@gmail.com>
2021-08-09 17:16:39 -0500

    progress on Bottom sheet

---
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/bottomMenus/AccountsBottomMenu.java
@@ -0,0 +1,29 @@
+package be.kuleuven.elcontador10.fragments.bottomMenus;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import be.kuleuven.elcontador10.R;
+
+
+public class AccountsBottomMenu extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_accounts_bottom_menu, container, false);
+    }
+}
