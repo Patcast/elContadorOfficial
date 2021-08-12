a77d61d2e46289d0b9bb777e174a147a6679eb24
patocastillo <patricio.castillo2009@gmail.com>
2021-08-11 19:20:40 -0500

    Add Accounts works perfectly

--- app/src/main/java/be/kuleuven/elcontador10/background/model/Account.java
+++ app/src/main/java/be/kuleuven/elcontador10/background/model/Account.java
@@ -1,12 +1,38 @@
 package be.kuleuven.elcontador10.background.model;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import be.kuleuven.elcontador10.background.database.Caching;
+
 public class Account {
+    private static final String TAG = "account";
     private long balance;
     private String name;
+    private ArrayList<String> users;
     private String id;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public Account( String name,long balance, ArrayList<String> users) {
+        this.balance = balance;
+        this.name = name;
+        this.users = users;
+    }
 
     public Account() {
     }
+
 
     public Account(long balance, String name, String id) {
         this.balance = balance;
@@ -18,6 +44,27 @@ public class Account {
         this.balance = balance;
         this.name = name;
     }
+
+    public void sendNewAccount(Account newAccount, Context context){
+        db.collection("accounts")
+                .add(newAccount)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                        Toast.makeText(context,"Account registered",Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+
 
     public void setId(String id) {
         this.id = id;
@@ -33,5 +80,9 @@ public class Account {
 
     public String getId() {
         return id;
+    }
+
+    public ArrayList<String> getUsers() {
+        return users;
     }
 }
--- app/src/main/java/be/kuleuven/elcontador10/fragments/accounts/AddNewAccount.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/accounts/AddNewAccount.java
@@ -2,19 +2,34 @@ package be.kuleuven.elcontador10.fragments.accounts;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 import be.kuleuven.elcontador10.R;
 import be.kuleuven.elcontador10.activities.MainActivity;
+import be.kuleuven.elcontador10.background.model.Account;
 
 
 public class AddNewAccount extends Fragment {
-
+    Button confirmButton;
+    EditText edTextName;
+    EditText edTextBalance;
     MainActivity mainActivity;
+    NavController navController;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +41,45 @@ public class AddNewAccount extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_new_account, container, false);
+        View view =inflater.inflate(R.layout.fragment_add_new_account, container, false);
+        confirmButton = view.findViewById(R.id.btn_confirm_NewAccount);
+        edTextBalance = view.findViewById(R.id.ed_txt_starting_balance);
+        edTextName = view.findViewById(R.id.ed_txt_name_Account);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        confirmButton.setOnClickListener(v ->registerAccount());
+    }
+
+    private void registerAccount() {
+        String loggedInEmail = mainActivity.returnSavedLoggedEmail();
+        if(loggedInEmail!=null){
+            ArrayList<String> users= new ArrayList<>();
+            users.add(loggedInEmail);
+            String nameOfAccount =edTextName.getText().toString();
+            if (!(nameOfAccount.isEmpty())){
+                String amountText = edTextBalance.getText().toString();
+                long amount;
+                if(!(amountText.isEmpty())) {
+                    amount = Long.parseLong(amountText);
+                }
+                else{amount =0;}
+                Account newAccount = new Account(nameOfAccount,amount,users);
+                newAccount.sendNewAccount(newAccount,getContext());
+            }
+            else{
+                Toast.makeText(getContext(),"Please type down a name for the account",Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(getContext(),"No Logged In email found",Toast.LENGTH_SHORT).show();
+
+        }
+        navController.popBackStack();
     }
 
     @Override
