3c222a143294b700553e708ce5c4c2a1a386130d
patocastillo <patricio.castillo2009@gmail.com>
2021-08-20 21:53:11 -0500

    upload pics from gallery

--- app/src/main/java/be/kuleuven/elcontador10/fragments/transactions/NewTransaction/TransactionNew.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/transactions/NewTransaction/TransactionNew.java
@@ -1,31 +1,44 @@
 package be.kuleuven.elcontador10.fragments.transactions.NewTransaction;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.graphics.Bitmap;
+
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import be.kuleuven.elcontador10.R;
@@ -44,15 +57,18 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
     public static final int CAMARA_PERM_CODE = 2901;
     private static final String TAG = "TransactionNew";
     public static final int CAMARA_REQUEST_CODE = 1382;
+    public static final int GALLERY_REQUEST_CODE = 3892;
     RadioGroup radGroup;
     TextView txtWordsCounterTitle,accountSelected,txtEmojiCategory,txtStakeHolder,txtWordsCounterNotes,txtMustHaveAmount;
     ImageButton btnAddCategory,btnAddPicture;
+    ImageView imageFinal;
     EditText txtAmount,txtTitle,txtNotes;
     MainActivity mainActivity;
     NavController navController;
     NewTransactionViewModel viewModel;
     StakeHolder selectedStakeHolder;
     String idCatSelected;
+    String currentPhotoPath;
 
 
     @Override
@@ -76,6 +92,7 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
         navController = Navigation.findNavController(view);
 
 ///      Initialize views
+        imageFinal =view.findViewById(R.id.imageView_final);
         btnAddPicture =view.findViewById(R.id.imageButton_addPicture);
         accountSelected = view.findViewById(R.id.textView_currentAccount);
         radGroup = view.findViewById(R.id.radioGroup);
@@ -88,9 +105,10 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
         txtNotes = view.findViewById(R.id.ed_txt_notes);
         txtWordsCounterNotes = view.findViewById(R.id.text_newTransaction_wordCounter_notes);
         txtMustHaveAmount = view.findViewById(R.id.textView_fillAmount);
-
+        Button galleryButton = view.findViewById(R.id.btn_add_pictureFromGallery);
         Button confirmButton = view.findViewById(R.id.btn_confirm_NewTransaction);
         // listeners
+        galleryButton.setOnClickListener(v->useGallery());
         txtStakeHolder.setOnClickListener(v -> { navController.navigate(R.id.action_newTransaction_to_chooseStakeHolderDialog); });
         confirmButton.setOnClickListener(v -> confirmTransaction());
         btnAddCategory.setOnClickListener(v-> navController.navigate(R.id.action_newTransaction_to_chooseCategory));
@@ -99,6 +117,7 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
         setWordCounters();
         accountSelected.setText(Caching.INSTANCE.getAccountName());
     }
+
 
 
     private void setWordCounters() {
@@ -216,11 +235,13 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
         EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @AfterPermissionGranted(CAMARA_PERM_CODE)
     private void askForCamaraPermission() {
-        String[] perms= {Manifest.permission.CAMERA};
+        String[] perms= {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE};
         if(EasyPermissions.hasPermissions(requireContext(),perms)){
-            openCamara();
+            //openCamara();
+            dispatchTakePictureIntent();
         }
         else{
             EasyPermissions.requestPermissions(this,getString(R.string.camara_permission_denied),CAMARA_PERM_CODE,perms);
@@ -228,10 +249,6 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
 
     }
 
-    private void openCamara() {
-        Intent camara = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camara, CAMARA_REQUEST_CODE);
-    }
 
     @Override
     public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
@@ -248,12 +265,91 @@ public class TransactionNew extends Fragment implements EasyPermissions.Permissi
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
+      /*  if(requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE){
 
-        }
+        }*/
         if(requestCode==CAMARA_REQUEST_CODE){
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            btnAddPicture.setImageBitmap(image);
+            /*Bitmap image = (Bitmap) data.getExtras().get("data");
+            btnAddPicture.setImageBitmap(image);*/
+            if(resultCode== Activity.RESULT_OK){
+                File f = new  File(currentPhotoPath);
+                imageFinal.setImageURI(Uri.fromFile(f));
+                galleryAddPic(f);
+            }
+        }
+        if(requestCode==GALLERY_REQUEST_CODE){
+            /*Bitmap image = (Bitmap) data.getExtras().get("data");
+            btnAddPicture.setImageBitmap(image);*/
+            if(resultCode== Activity.RESULT_OK){
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_"+timeStamp+"."+getFileExt(contentUri);
+                Log.d("tag","onActivityResult: Gallery Image Uri: "+imageFileName);
+                imageFinal.setImageURI(contentUri);
+
+            }
+        }
+
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = requireActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+    private void galleryAddPic(File f) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+       // File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES); USE if you don't want them to be saved in the gallery.
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO=1;
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(requireActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(requireContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMARA_REQUEST_CODE);
+            }
         }
     }
+    private void useGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, GALLERY_REQUEST_CODE);
+    }
+
 }
