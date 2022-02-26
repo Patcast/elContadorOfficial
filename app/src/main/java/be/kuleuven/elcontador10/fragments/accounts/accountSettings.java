4cbaa88b14be18af6c5e3df1250cc7a6183e2629
patocastillo <patricio.castillo2009@gmail.com>
2022-02-26 14:12:50 +0100

    brush xml and skeleton of fragment

--- app/src/main/java/be/kuleuven/elcontador10/fragments/accounts/accountSettings.java
+++ app/src/main/java/be/kuleuven/elcontador10/fragments/accounts/accountSettings.java
@@ -1,14 +1,24 @@
 package be.kuleuven.elcontador10.fragments.accounts;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import be.kuleuven.elcontador10.R;
+import be.kuleuven.elcontador10.activities.MainActivity;
+import be.kuleuven.elcontador10.background.database.Caching;
 
 
 public class accountSettings extends Fragment {
@@ -19,13 +29,27 @@ public class accountSettings extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        MainActivity mainActivity = (MainActivity) getActivity();
+        mainActivity.setHeaderText(getString(R.string.account_settings));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         return inflater.inflate(R.layout.fragment_account_settings, container, false);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText edTextNewEmail = view.findViewById(R.id.edTextEmailPart);
+        Button btnAddEmail = view.findViewById(R.id.butAddEmail);
+        RecyclerView recParticipants = view.findViewById(R.id.recParticipants);
+        TextView textAccountName = view.findViewById(R.id.textAccountName);
+        textAccountName.setText(Caching.INSTANCE.getAccountName());
+    }
+
+
 }
