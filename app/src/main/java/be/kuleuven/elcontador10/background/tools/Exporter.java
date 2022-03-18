078dcd5d789b9bfbbbc55dd1ed1733c723267604
elYonno <78961541+elYonno@users.noreply.github.com>
2022-03-18 14:28:50 +0100

    late transactions to excel

--- app/src/main/java/be/kuleuven/elcontador10/background/tools/Exporter.java
+++ app/src/main/java/be/kuleuven/elcontador10/background/tools/Exporter.java
@@ -6,11 +6,14 @@ import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 
+import com.google.firebase.Timestamp;
+
 import org.apache.poi.hssf.usermodel.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -61,10 +64,12 @@ public enum Exporter {
 
         summarySheet(workbook, styleCurrency, startingBalance, cashIn, cashOut, currentBalance, receivables, payables, scheduleBalance);
         transactionsSheet(workbook, styleCurrency, processed);
+        lateSheet(workbook, styleCurrency, scheduled);
 
         return workbook;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void summarySheet(HSSFWorkbook workbook, HSSFCellStyle styleCurrency, long startingBalance, long cashIn, long cashOut, long currentBalance, long receivables,
                               long payables, long scheduleBalance) {
         HSSFSheet sheet = workbook.createSheet("Summary");
@@ -117,13 +122,17 @@ public enum Exporter {
         cell = row.createCell(1);
         cell.setCellValue(scheduleBalance);
         cell.setCellStyle(styleCurrency);
+
+        row = sheet.createRow(9);
+        cell = row.createCell(0);
+        cell.setCellValue(MessageFormat.format("Exported by {0} at {1}",
+                Caching.INSTANCE.getAccountName(), DatabaseDatesFunctions.INSTANCE.timestampToString(Timestamp.now())));
     }
 
-    // todo incude categories
     /**
-     * Date | title | totalAmount | Stakeholder | Registered by | Notes
-     * -----|-------|-------------|-------------|---------------|-------
-     *      |       |             |             |               |
+     * Date | title | totalAmount | Category | Stakeholder | Registered by | Notes
+     * -----|-------|-------------|----------|-------------|---------------|-------
+     *      |       |             |          |             |               |
      */
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void transactionsSheet(HSSFWorkbook workbook, HSSFCellStyle styleCurrency, List<ProcessedTransaction> processed) {
@@ -143,10 +152,12 @@ public enum Exporter {
         cell = row.createCell(2);
         cell.setCellValue("Amount ($)");
         cell = row.createCell(3);
-        cell.setCellValue("Stakeholder");
+        cell.setCellValue("Category");
         cell = row.createCell(4);
-        cell.setCellValue("Registered by");
+        cell.setCellValue("Stakeholder");
         cell = row.createCell(5);
+        cell.setCellValue("Registered by");
+        cell = row.createCell(6);
         cell.setCellValue("Notes");
 
         List<ProcessedTransaction> income = processed.stream()
@@ -170,12 +181,15 @@ public enum Exporter {
             cell.setCellStyle(styleCurrency);
 
             cell = row.createCell(3);
-            cell.setCellValue(Caching.INSTANCE.getStakeholderName(transaction.getIdOfStakeInt()));
+            cell.setCellValue(Caching.INSTANCE.getCategoryTitle(transaction.getIdOfCategoryInt()));
 
             cell = row.createCell(4);
-            cell.setCellValue(transaction.getRegisteredBy());
+            cell.setCellValue(Caching.INSTANCE.getStakeholderName(transaction.getIdOfStakeInt()));
 
             cell = row.createCell(5);
+            cell.setCellValue(transaction.getRegisteredBy());
+
+            cell = row.createCell(6);
             cell.setCellValue(transaction.getNotes());
 
             counter++;
@@ -183,22 +197,24 @@ public enum Exporter {
 
         // expenses
         row = sheet.getRow(0);
-        cell = row.createCell(7);
+        cell = row.createCell(8);
 
         cell.setCellValue("Expenses");
 
         row = sheet.getRow(1);
-        cell = row.createCell(7);
-        cell.setCellValue("Date & Time");
         cell = row.createCell(8);
-        cell.setCellValue("Title");
+        cell.setCellValue("Date & Time");
         cell = row.createCell(9);
-        cell.setCellValue("Amount ($)");
+        cell.setCellValue("Title");
         cell = row.createCell(10);
-        cell.setCellValue("Stakeholder");
+        cell.setCellValue("Amount ($)");
         cell = row.createCell(11);
-        cell.setCellValue("Registered by");
+        cell.setCellValue("Category");
         cell = row.createCell(12);
+        cell.setCellValue("Stakeholder");
+        cell = row.createCell(13);
+        cell.setCellValue("Registered by");
+        cell = row.createCell(14);
         cell.setCellValue("Notes");
 
         List<ProcessedTransaction> expenses = processed.stream()
@@ -211,26 +227,175 @@ public enum Exporter {
         for (ProcessedTransaction transaction : expenses) {
             row = sheet.getRow(counter);
 
-            cell = row.createCell(7);
+            if (row == null) row = sheet.createRow(counter);
+
+            cell = row.createCell(8);
             cell.setCellValue(DatabaseDatesFunctions.INSTANCE.timestampToStringDetailed(transaction.getDueDate()));
 
             cell = row.createCell(9);
             cell.setCellValue(transaction.getTitle());
 
-            cell = row.createCell(9);
-            cell.setCellValue(transaction.getTotalAmount());
+            cell = row.createCell(10);
+            cell.setCellValue(Math.abs(transaction.getTotalAmount()));
             cell.setCellStyle(styleCurrency);
 
-            cell = row.createCell(10);
-            cell.setCellValue(Caching.INSTANCE.getStakeholderName(transaction.getIdOfStakeInt()));
-
             cell = row.createCell(11);
-            cell.setCellValue(transaction.getRegisteredBy());
+            cell.setCellValue(Caching.INSTANCE.getCategoryTitle(transaction.getIdOfCategoryInt()));
 
             cell = row.createCell(12);
+            cell.setCellValue(Caching.INSTANCE.getStakeholderName(transaction.getIdOfStakeInt()));
+
+            cell = row.createCell(13);
+            cell.setCellValue(transaction.getRegisteredBy());
+
+            cell = row.createCell(14);
             cell.setCellValue(transaction.getNotes());
 
             counter++;
         }
+    }
+
+    /**
+     * Stakeholder | title | amountPaid | totalAmount | dueDate | Category | Registered by
+     * ------------|-------|------------|-------------|---------|----------|---------------
+     */
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void lateSheet(HSSFWorkbook workbook, HSSFCellStyle styleCurrency, List<ScheduledTransaction> scheduled) {
+        HSSFSheet sheet = workbook.createSheet("Incomplete schedule transactions");
+        List<ScheduledTransaction> late = scheduled.stream()
+                .filter(t -> t.getStatus() == ScheduledTransaction.ScheduledTransactionStatus.LATE)
+                .collect(Collectors.toList());
+
+
+        // receivables
+        List<ScheduledTransaction> receivables = late.stream()
+                .filter(t -> t.getTotalAmount() > 0)
+                .collect(Collectors.toList());
+
+        HSSFRow row = sheet.createRow(0);
+        HSSFCell cell = row.createCell(0);
+        cell.setCellValue("Receivables");
+
+        row = sheet.createRow(1);
+        cell = row.createCell(0);
+        cell.setCellValue("Stakeholder");
+        cell = row.createCell(1);
+        cell.setCellValue("Title");
+        cell = row.createCell(2);
+        cell.setCellValue("Amount Paid ($)");
+        cell = row.createCell(3);
+        cell.setCellValue("Amount to Pay ($)");
+        cell = row.createCell(4);
+        cell.setCellValue("Due date");
+        cell = row.createCell(5);
+        cell.setCellValue("Category");
+        cell = row.createCell(6);
+        cell.setCellValue("Registered by");
+
+        int totalAmountToReceive = 0;
+        int counter = 2;
+
+        for (ScheduledTransaction transaction : receivables) {
+            row = sheet.createRow(counter);
+
+            cell = row.createCell(0);
+            cell.setCellValue(Caching.INSTANCE.getStakeholderName(transaction.getIdOfStakeholder()));
+
+            cell = row.createCell(1);
+            cell.setCellValue(transaction.getTitle());
+
+            cell = row.createCell(2);
+            cell.setCellValue(transaction.getAmountPaid());
+            cell.setCellStyle(styleCurrency);
+
+            cell = row.createCell(3);
+            cell.setCellValue(transaction.getTotalAmount());
+            cell.setCellStyle(styleCurrency);
+
+            cell = row.createCell(4);
+            cell.setCellValue(DatabaseDatesFunctions.INSTANCE.timestampToStringDetailed(transaction.getDueDate()));
+
+            cell = row.createCell(5);
+            cell.setCellValue(Caching.INSTANCE.getCategoryTitle(transaction.getCategory()));
+
+            cell = row.createCell(6);
+            cell.setCellValue(transaction.getIdOfAccount());
+
+            totalAmountToReceive += transaction.getTotalAmount() - transaction.getAmountPaid();
+            counter++;
+        }
+
+        row = sheet.createRow(counter);
+        cell = row.createCell(0);
+        cell.setCellValue("Total late receivables ($):");
+        cell = row.createCell(1);
+        cell.setCellValue(totalAmountToReceive);
+
+        // payables
+        List<ScheduledTransaction> payables = late.stream()
+                .filter(t ->t.getTotalAmount() < 0)
+                .collect(Collectors.toList());
+
+        row = sheet.getRow(0);
+        cell = row.createCell(8);
+        cell.setCellValue("Payables");
+
+        row = sheet.getRow(1);
+        cell = row.createCell(8);
+        cell.setCellValue("Stakeholder");
+        cell = row.createCell(9);
+        cell.setCellValue("Title");
+        cell = row.createCell(10);
+        cell.setCellValue("Amount Paid ($)");
+        cell = row.createCell(11);
+        cell.setCellValue("Amount to Pay ($)");
+        cell = row.createCell(12);
+        cell.setCellValue("Due date");
+        cell = row.createCell(13);
+        cell.setCellValue("Category");
+        cell = row.createCell(14);
+        cell.setCellValue("Registered by");
+
+        int totalAmountToPay = 0;
+        counter = 2;
+
+        for (ScheduledTransaction transaction : payables) {
+            row = sheet.getRow(counter);
+            if (row == null) row = sheet.createRow(counter);
+
+            cell = row.createCell(8);
+            cell.setCellValue(Caching.INSTANCE.getStakeholderName(transaction.getIdOfStakeholder()));
+
+            cell = row.createCell(9);
+            cell.setCellValue(transaction.getTitle());
+
+            cell = row.createCell(10);
+            cell.setCellValue(transaction.getAmountPaid());
+            cell.setCellStyle(styleCurrency);
+
+            cell = row.createCell(11);
+            cell.setCellValue(transaction.getTotalAmount());
+            cell.setCellStyle(styleCurrency);
+
+            cell = row.createCell(12);
+            cell.setCellValue(DatabaseDatesFunctions.INSTANCE.timestampToStringDetailed(transaction.getDueDate()));
+
+            cell = row.createCell(13);
+            cell.setCellValue(Caching.INSTANCE.getCategoryTitle(transaction.getCategory()));
+
+            cell = row.createCell(14);
+            cell.setCellValue(transaction.getIdOfAccount());
+
+            totalAmountToPay += transaction.getTotalAmount() - transaction.getAmountPaid();
+            counter++;
+        }
+
+        row = sheet.getRow(counter);
+        if (row == null) row = sheet.createRow(counter);
+        cell = row.createCell(9);
+        cell.setCellValue("Total late payables ($):");
+        cell = row.createCell(9);
+        cell.setCellValue(totalAmountToPay);
     }
 }
